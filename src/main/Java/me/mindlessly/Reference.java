407062ea3e5fc717f21804e0b7dda10ca5ba0eca
FlippingEternal <104845054+EternalFlipping@users.noreply.github.com>
2022-05-03 19:58:09 +0000

    fix priority ordering on modifiers

---
+++ src/main/Java/me/mindlessly/Reference.java
@@ -0,0 +1,12 @@
+package me.mindlessly.notenoughcoins;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Reference {
+
+    public static final String MOD_ID = "FlippingEternal";
+    public static final String NAME = "FlippingEternal";
+    public static final String VERSION = "v@version@";
+    public static final Logger logger = LogManager.getLogger("FlippingEternal");
+}
